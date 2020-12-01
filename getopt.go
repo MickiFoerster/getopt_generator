a4c4b33430f09d5f94da2f7a7e679440e3016098
micki <github@com-science.de>
2020-12-01 07:53:25 +0100

    Apply clang-format after code generation

--- getopt.go
+++ getopt.go
@@ -161,7 +161,7 @@ Options :\n\
 	}
 	f.Close()
 	// Postprocess with clang-format
-	//applyClangFormat(fn)
+	applyClangFormat(fn)
 
 	fmt.Println("Now, we test this code by using:")
 	testOutputWithCompiler("gcc", fn)
